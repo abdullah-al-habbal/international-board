dde28a4a1472e492c6eba7df19d787eaa0672066
Abdullah Al Habal <dbnkalhbalb@outlook.sa>
2025-11-30 22:17:27 +0300

    Enhance local check scripts in composer.json and package.json; update pre-commit and pre-push hooks to use check-local instead of check-all. Remove unused .gitignore from bootstrap/cache.

---
+++ bootstrap/cache/filament/panels/center.php
@@ -0,0 +1,74 @@
+<?php return array (
+  'livewireComponents' => 
+  array (
+    'app.filament.center.resources.accreditation-requests.pages.create-accreditation-request' => 'App\\Filament\\Center\\Resources\\AccreditationRequests\\Pages\\CreateAccreditationRequest',
+    'app.filament.center.resources.accreditation-requests.pages.edit-accreditation-request' => 'App\\Filament\\Center\\Resources\\AccreditationRequests\\Pages\\EditAccreditationRequest',
+    'app.filament.center.resources.accreditation-requests.pages.list-accreditation-requests' => 'App\\Filament\\Center\\Resources\\AccreditationRequests\\Pages\\ListAccreditationRequests',
+    'app.filament.center.resources.accreditation-requests.pages.view-accreditation-request' => 'App\\Filament\\Center\\Resources\\AccreditationRequests\\Pages\\ViewAccreditationRequest',
+    'app.filament.center.resources.certifications.pages.create-certification' => 'App\\Filament\\Center\\Resources\\Certifications\\Pages\\CreateCertification',
+    'app.filament.center.resources.certifications.pages.edit-certification' => 'App\\Filament\\Center\\Resources\\Certifications\\Pages\\EditCertification',
+    'app.filament.center.resources.certifications.pages.list-certifications' => 'App\\Filament\\Center\\Resources\\Certifications\\Pages\\ListCertifications',
+    'app.filament.center.resources.certifications.pages.view-certification' => 'App\\Filament\\Center\\Resources\\Certifications\\Pages\\ViewCertification',
+    'filament.pages.dashboard' => 'Filament\\Pages\\Dashboard',
+    'app.filament.center.widgets.center-stats-overview' => 'App\\Filament\\Center\\Widgets\\CenterStatsOverview',
+    'app.filament.center.widgets.monthly-certifications-chart' => 'App\\Filament\\Center\\Widgets\\MonthlyCertificationsChart',
+    'filament.livewire.database-notifications' => 'Filament\\Livewire\\DatabaseNotifications',
+    'filament.auth.pages.edit-profile' => 'Filament\\Auth\\Pages\\EditProfile',
+    'filament.livewire.global-search' => 'Filament\\Livewire\\GlobalSearch',
+    'filament.livewire.notifications' => 'Filament\\Livewire\\Notifications',
+    'filament.livewire.sidebar' => 'Filament\\Livewire\\Sidebar',
+    'filament.livewire.simple-user-menu' => 'Filament\\Livewire\\SimpleUserMenu',
+    'filament.livewire.topbar' => 'Filament\\Livewire\\Topbar',
+    'filament.auth.pages.login' => 'Filament\\Auth\\Pages\\Login',
+  ),
+  'clusters' => 
+  array (
+  ),
+  'clusteredComponents' => 
+  array (
+  ),
+  'clusterDirectories' => 
+  array (
+  ),
+  'clusterNamespaces' => 
+  array (
+  ),
+  'pages' => 
+  array (
+    0 => 'Filament\\Pages\\Dashboard',
+  ),
+  'pageDirectories' => 
+  array (
+    0 => 'D:\\work\\board\\app\\Filament/Center/Pages',
+  ),
+  'pageNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Center\\Pages',
+  ),
+  'resources' => 
+  array (
+    'D:\\work\\board\\app\\Filament\\Center\\Resources\\AccreditationRequests\\AccreditationRequestResource.php' => 'App\\Filament\\Center\\Resources\\AccreditationRequests\\AccreditationRequestResource',
+    'D:\\work\\board\\app\\Filament\\Center\\Resources\\Certifications\\CertificationResource.php' => 'App\\Filament\\Center\\Resources\\Certifications\\CertificationResource',
+  ),
+  'resourceDirectories' => 
+  array (
+    0 => 'D:\\work\\board\\app\\Filament/Center/Resources',
+  ),
+  'resourceNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Center\\Resources',
+  ),
+  'widgets' => 
+  array (
+    'D:\\work\\board\\app\\Filament\\Center\\Widgets\\CenterStatsOverview.php' => 'App\\Filament\\Center\\Widgets\\CenterStatsOverview',
+    'D:\\work\\board\\app\\Filament\\Center\\Widgets\\MonthlyCertificationsChart.php' => 'App\\Filament\\Center\\Widgets\\MonthlyCertificationsChart',
+  ),
+  'widgetDirectories' => 
+  array (
+    0 => 'D:\\work\\board\\app\\Filament/Center/Widgets',
+  ),
+  'widgetNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Center\\Widgets',
+  ),
+);
