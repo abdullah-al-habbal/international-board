217a08b4c2f3306444e1bf077424f61d4f5c0e57
abdullah_al_habal <dbnkalhbalb@gmail.com>
2026-07-17 23:19:29 +0300

    refactor: clean up whitespace in CenterAccreditationRequestResource.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,36 +11,25 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // Config-based seeders (deterministic data)
             CountrySeeder::class,
             ApplicationSettingSeeder::class,
             DocumentTypeSeeder::class,
-
-            // Independent models
             UserSeeder::class,
             StaticPageSeeder::class,
             MembershipSeeder::class,
             BlogPostSeeder::class,
             ContactMessageSeeder::class,
-
-            // Models depending on basic data
             CertifiedCenterSeeder::class,
             TrainerSeeder::class,
             TraineeSeeder::class,
-
-            // Center-dependent models
             CertifiedCenterPaymentAgentPersonSeeder::class,
             CertifiedCenterDocumentTypeSeeder::class,
             CenterTypeRequestSeeder::class,
             CertifiedCenterFinancialRequestSeeder::class,
             AccreditationRequestSeeder::class,
-
-            // Trainer-dependent models
             TrainerAccreditationRequestSeeder::class,
             TrainerDocumentTypeSeeder::class,
             TrainerFinancialRequestSeeder::class,
-
-            // Models depending on everything above
             CertificationSeeder::class,
         ]);
     }
